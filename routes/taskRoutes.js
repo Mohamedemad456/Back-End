c85420a677712f21b4018a63adfb28eb1f07e204
Mohamedemad456 <mohamed.asker316@gmail.com>
2024-10-13 16:20:20 +0300

    Handling Filtering

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 
-const {getTasks, getTaskById, getTasksByCategoryId , createTask, updateTask, deleteTask} = require('../controllers/taskControllers');
+const {getTasks, getTaskById, getTasksByCategoryId , createTask, updateTask, deleteTask, filterTasks} = require('../controllers/taskControllers');
 
-router.get('/',getTasks);
-router.get('/:id',getTaskById);
+router.get('/filter',filterTasks);
 router.get('/category/:categoryId',getTasksByCategoryId);
+router.get('/:id',getTaskById);
+router.get('/',getTasks);
 router.post('/',createTask);
 router.put('/:id',updateTask);
 router.delete('/:taskId/:workSpaceId',deleteTask);
